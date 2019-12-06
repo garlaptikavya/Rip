4bdab086bdf241ef27902b775d366d2366b0d569
garlapatikavya <kavyagarlapati45@gmail.com>
2019-12-06 22:32:20 +0530

    kavu

--- app.py
+++ app.py
@@ -1,9 +1,30 @@
 from flask import Flask
 app=Flask(__name__)
+app.config["DEBUG"]=True
 @app.route("/")
 @app.route("/hello")
 def hello_world():
 	return "HELLO WORLD ! I LOVE YOU !"
+@app.route("/test/<search_query>")
+def search(search_query):
+	return search_query
+@app.route("/integer/<int:value>")
+def int_type(value):
+	print(value+1)
+	return("I I 143")
+@app.route("/float/<float:value>")
+def float_type(value):
+	print(value+1)
+	return "I love ...."
+@app.route("/path/<path:value>")
+def path_typ(value):
+	print(value)
+	return "correct"
+@app.route("/name/<name>")
+def index(name):
+	if name.lower()=="michael":
+		return "Hello ,{}".format(name), 200
+	else:
+		return "not found",404
 if __name__=="__main__":
 	app.run()
-	
